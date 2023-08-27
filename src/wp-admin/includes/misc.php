2531b9de0918bb180dd47f4e22aaedd7d3dd248c
= <=>
2023-08-27 02:53:11 +0200

    Fix add_query_arg.

--- src/wp-admin/includes/misc.php
+++ src/wp-admin/includes/misc.php
@@ -1394,7 +1394,6 @@ function wp_admin_canonical_url() {
 	}
 
 	// Ensure we're using an absolute URL.
-	$current_url  = set_url_scheme( 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] );
 	$filtered_url = remove_query_arg( $removable_query_args );
 
 	/**
